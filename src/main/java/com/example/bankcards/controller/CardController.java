d07877fd1f9bb299c542ef959e787b9d8b6a3111
lomuserg <sergeymu2015@yandex.ru>
2025-10-12 17:21:01 +0300

    CardController

--- src/main/java/com/example/bankcards/controller/CardController.java
+++ src/main/java/com/example/bankcards/controller/CardController.java
@@ -1,9 +1,11 @@
 package com.example.bankcards.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1/cards")
+@RestController
+@RequestMapping("api/v1/cards")
 @RequiredArgsConstructor
 public class CardController {
 }
