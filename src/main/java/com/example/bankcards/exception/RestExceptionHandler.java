cdd99c676dad2b3704470bfab816d7ea1887d4d1
lomuserg <sergeymu2015@yandex.ru>
2025-10-12 16:51:56 +0300

    error handler

---
+++ src/main/java/com/example/bankcards/exception/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.bankcards.exception;
+
+import com.example.bankcards.dto.ErrorDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(value = { AppException.class })
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleException(AppException ex) {
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(ErrorDto.builder().message(ex.getMessage()).build());
+    }
+}
